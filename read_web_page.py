3b177bae4f436237455c2cd0b16ac3e9bbc50ce2
IurieChi <chigaiiura@yahoo.com>
2023-08-25 12:09:41 +0200

    read page logic added

---
+++ read_web_page.py
@@ -0,0 +1,57 @@
+import requests 
+from bs4 import BeautifulSoup
+
+# data form 
+# pagina: domenii
+# cod: 48190392
+# captcha: dew
+# B1: VIZUALIZARE
+
+# URL of the target page
+search_url = 'https://mfinante.gov.ro/domenii/informatii-contribuabili/persoane-juridice/info-pj-selectie-dupa-cui'
+sorter_url = 'https://mfinante.gov.ro/apps/infocodfiscal.html'
+destination_url ='https://mfinante.gov.ro/apps/infocodfiscal.html;jsessionid=ztHxEIUcR8-o3JqG_Osi727iZvHZWsWoZrgoo9jo.apps4'
+
+
+
+# # Sample data for ID and CAPTCHA (replace with actual data)
+# id_value = '48190392' #"your_id_here"
+# captcha_value = "captcha_solution_here"
+
+# Set up the session
+with requests.Session() as s:
+    # Perform an initial request to retrieve the page content
+    response = s.get(sorter_url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    print(soup)
+    
+
+
+
+
+# # Find the CAPTCHA input element and set its value
+# captcha_input = soup.find("input", {"name": "captcha"})
+# if captcha_input:
+#     captcha_input["value"] = captcha_value
+
+# Find the ID input element and set its value
+# id_input = soup.find("input", {"name": "cod"})
+# if id_input:
+#     id_input["value"] = id_value
+
+# print(soup.text)
+
+
+# # Submit the form
+# form = soup.find("form")
+# if form:
+#     response = session.post(url, data=form)
+
+# # Parse and extract the result
+# result_soup = BeautifulSoup(response.content, "html.parser")
+# result_data = result_soup.find("div", {"id": "content"})
+
+# if result_data:
+#     print(result_data.get_text())
+# else:
+#     print("No result data found.")
