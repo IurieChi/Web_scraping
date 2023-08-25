136e284ef2cf7bbfd237e6ca77f93e8d526e14f8
IurieChi <chigaiiura@yahoo.com>
2023-08-25 14:51:15 +0200

    read data from page

--- read_web_page.py
+++ read_web_page.py
@@ -8,28 +8,38 @@ from bs4 import BeautifulSoup
 # B1: VIZUALIZARE
 
 # URL of the target page
-search_url = 'https://mfinante.gov.ro/domenii/informatii-contribuabili/persoane-juridice/info-pj-selectie-dupa-cui'
-sorter_url = 'https://www.google.com/'#'https://mfinante.gov.ro/apps/infocodfiscal.html'
+search_url = 'https://mfinante.gov.ro/despre-minister'
+sorter_url = 'https://mfinante.gov.ro/apps/infocodfiscal.html'
 destination_url ='https://mfinante.gov.ro/apps/infocodfiscal.html;jsessionid=ztHxEIUcR8-o3JqG_Osi727iZvHZWsWoZrgoo9jo.apps4'
 
-
+headers = {
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko)"
+}
 
 # # Sample data for ID and CAPTCHA (replace with actual data)
 id_value = '48190392' #"your_id_here"
-captcha_value = "captcha_solution_here"
+captcha_value = "captcha"
 
-# Set up the session
-with requests.Session() as s:
-    # Perform an initial request to retrieve the page content
-    response = s.get(search_url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    print(response)
+716
+
+try:
+    # Set up the session
+    with requests.Session() as s:
+        # Perform an initial request to retrieve the page content
+        response = s.get(sorter_url, headers=headers, timeout=10)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        # s.raise_for_status()
+        
+except requests.exceptions.HTTPError as e:
+    print (e.response.text)
     
-# # Find the ID input element by class name and set its value
-# id_input = soup.find('input', class_='form2')
-# if id_input:
-#     id_input['value'] = id_value
+# Find the ID input element by class name and set its value
+id_input = soup.find('input', class_='form2')
+if id_input:
+    id_input['value'] = id_value
 
+print(id_input)
 # # Find the CAPTCHA input element by class name and set its value
 # captcha_input = soup.find('input', name='captcha')
 # if captcha_input:
